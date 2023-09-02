c110a1e474f21560dd260470eafdf316a48110d9
cembdc <cembdci@gmail.com>
2023-09-02 16:41:42 +0300

    json array to struct

--- main.go
+++ main.go
@@ -35,27 +35,41 @@ func main() {
 
 func dynamicJson(input []byte) {
 
-	var msg json.RawMessage
-	conf := Config{
-		SourceSettings: &msg,
-	}
+	var agent Agent
 
-	if err := json.Unmarshal([]byte(input), &conf); err != nil {
+	if err := json.Unmarshal([]byte(input), &agent); err != nil {
 		log.Fatal(err)
 	}
-	switch conf.Source {
-	case "mqtt":
-		var mqttconf Mqttconfig
-		if err := json.Unmarshal(msg, &mqttconf); err != nil {
-			log.Fatal(err)
+
+	// fmt.Printf("\n\n json object:::: %+v", agent)
+
+	for i := 0; i < len(agent.Configs); i++ {
+		// var msg json.RawMessage
+		// aa := agent.Configs[i]{
+		// 	SourceSettings: &msg,
+		// }
+
+		conf := agent.Configs[i]
+		// conf.SourceSettings = &msg
+
+		switch conf.Source {
+		case "mqtt":
+			var mqttconf Mqttconfig
+			jsonString, _ := json.Marshal(conf.SourceSettings)
+
+			if err := json.Unmarshal(jsonString, &mqttconf); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(conf.Direction)
+			fmt.Println(mqttconf.Host)
+			fmt.Println(mqttconf.Port)
+			fmt.Println(mqttconf.ClientId)
+			fmt.Println(mqttconf.Topic)
+			fmt.Println(mqttconf.User)
+			fmt.Println(mqttconf.Password)
+		default:
+			log.Fatalf("unknown message type: %q", conf.Source)
 		}
-		// fmt.Printf("\n\n json object:::: %+v", mqttconf)
-		fmt.Println(mqttconf.Host)
-		fmt.Println(mqttconf.Port)
-		fmt.Println(mqttconf.ClientId)
-		fmt.Println(mqttconf.User)
-		fmt.Println(mqttconf.Password)
-	default:
-		log.Fatalf("unknown message type: %q", conf.Source)
 	}
+
 }
--- structs.go
+++ structs.go
@@ -6,10 +6,15 @@ type Mqttconfig struct {
 	ClientId string `json:"clientId"`
 	User     string `json:"user"`
 	Password string `json:"password"`
+	Topic    string `json:"topic"`
 }
 
 type Config struct {
 	Direction      string `json:"direction"`
 	Source         string `json:"source"`
 	SourceSettings interface{}
+}
+
+type Agent struct {
+	Configs []Config `json:"configs"`
 }
